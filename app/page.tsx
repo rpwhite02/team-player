13b9f53297cf078648e176f6ea31180cf2e54600
rpwhite02 <rpwhite02@gmail.com>
2024-10-21 20:35:49 -0700

    added who the hell is this button

--- app/page.tsx
+++ app/page.tsx
@@ -158,6 +158,11 @@ export default function Component() {
             > 
               Generate New Hint
             </button>
+            <button
+              className="rounded-full border bg-gray-800 border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
+            > 
+              Who the Hell is This
+            </button>
           </div>
         </div>
       </main>
